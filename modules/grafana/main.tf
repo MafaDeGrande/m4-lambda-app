eb9a447794edcdb6c9e66743f1e07551da5fc89d
Ivan Popov <mafakasik@gmail.com>
2024-11-01 17:59:40 +0200

    Add dashboard json

--- modules/grafana/main.tf
+++ modules/grafana/main.tf
@@ -3,6 +3,7 @@ resource "aws_grafana_workspace" "lambda_monitor" {
   account_access_type      = "CURRENT_ACCOUNT"
   authentication_providers = ["AWS_SSO"]
   permission_type          = "SERVICE_MANAGED"
+  grafana_version = "9.4"
   data_sources = ["CLOUDWATCH"]
   role_arn = aws_iam_role.assume.arn
 }
@@ -14,7 +15,7 @@ resource "aws_grafana_role_association" "role" {
 }
 
 resource "aws_iam_role" "assume" {
-  name = "grafana-assume"
+  name = var.name
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -26,6 +27,27 @@ resource "aws_iam_role" "assume" {
           Service = "grafana.amazonaws.com"
         }
       },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "grafana_cloudwatch_access" {
+  name = var.name
+  role = aws_iam_role.assume.name
+
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:GetMetricData",
+          "cloudwatch:ListMetrics",
+          "cloudwatch:GetMetricStatistics",
+          "cloudwatch:DescribeAlarms"
+        ]
+        Resource = "*"
+      }
     ]
   })
 }
